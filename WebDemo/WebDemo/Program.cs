3547c7fe39589ba1d2e3992b5cf73ddf0766955d
lsw2012abc <503801503@qq.com>
2019-02-19 17:42:38 +0800

    1

--- WebDemo/WebDemo/Program.cs
+++ WebDemo/WebDemo/Program.cs
@@ -19,6 +19,6 @@ namespace WebDemo
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>().UseUrls("http://*:5000");
     }
 }
--- WebDemo/WebDemo/Startup.cs
+++ WebDemo/WebDemo/Startup.cs
@@ -50,6 +50,7 @@ namespace WebDemo
             app.UseCookiePolicy();
 
             app.UseMvc();
+            
         }
     }
 }
